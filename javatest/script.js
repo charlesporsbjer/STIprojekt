2209f143196e9ab1a7fa14ad75f18386afa03696
charlesporsbjer <118073335+charlesporsbjer@users.noreply.github.com>
2024-03-05 17:16:42 +0100

    more in javascript

--- javatest/script.js
+++ javatest/script.js
@@ -2,8 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const changeMessageButton = document.getElementById('change-message');
     const welcomeSection = document.getElementById('welcome-section');
 
+    let welcomeMessageToggled = false; // Boolean true or false (1/0).
+
     changeMessageButton.addEventListener('click', function() {
-        welcomeSection.querySelector('h2').textContent = "You've changed the message!";
-        welcomeSection.querySelector('p').textContent = "Thanks for interacting with our website.";
+        if (welcomeMessageToggled) {
+            welcomeSection.querySelector('h2').textContent = "Hello, visitor!";
+            welcomeSection.querySelector('p').textContent = "Click the button below to change this message.";
+        } else {
+            welcomeSection.querySelector('h2').textContent = "You've changed the message!";
+            welcomeSection.querySelector('p').textContent = "Thanks for interacting with our website.";
+        }
+        welcomeMessageToggled = !welcomeMessageToggled; // Toggle the welcome message state
     });
 });
